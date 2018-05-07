7bebb78a7975959ca8b0da8f917b08d429a9f588
Reginaldo Santos <rmsantos@br.ibm.com>
2018-05-07 08:45:24 -0300

    creating Footer component

---
+++ src/componentes/Rodape.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Rodape = props => {
+    return (
+        <footer className='container-fluid text-center'>
+                <a href='#myPage' title='To Top'>
+                    <span className='glyphicon glyphicon-chevron-up'></span>
+                </a>
+                <p>Site criado no curso do Reginaldo Santos (Xumes) usando <a href='https://www.reginaldosantos.com.br' title='Xumes'>www.reginaldosantos.com.br</a></p>
+                <p> template Bootstrap Theme Made By <a href='https://www.w3schools.com' title='Visit w3schools'>www.w3schools.com</a></p>
+            </footer>
+    )
+}
+
+export default Rodape
