ba633ec0806734ec29e561362d06ecf4def86c5c
Reginaldo Santos <rmsantos@br.ibm.com>
2018-05-07 08:44:53 -0300

    moving HTML to JSX

---
+++ src/Site.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Site = props => {
+    return (
+        <div>
+        
+
+
+
+            {/* Container (Contact Section) */}
+            
+
+            
+        </div>
+    )
+}
+
+export default Site
